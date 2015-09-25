1004ae2bbfe35c30570d6c2b90e3c9df29087904
Vadim Graboviy <hraboviyvadim@gmail.com>
2015-09-25 12:28:29 +0300

    gulp template, fonts

---
+++ src/js/common.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	
+	// $(document).on("click", function(){
+	// 	$(".js-popup").hide();
+	// });
+
+	// function scrollFixedElements() {
+	//     var scroll_left = $(this).scrollLeft();
+	//     $(".fixed-element").css({
+	//         left: -scroll_left
+	//     });
+	// }
+	// scrollFixedElements();
+	// $(window).scroll(function(){
+	//     scrollFixedElements()
+	// });
+
+});
