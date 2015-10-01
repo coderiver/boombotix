16fc3fe73a7afd21cbae50b391733151b7896466
Vadim Graboviy <hraboviyvadim@gmail.com>
2015-10-01 18:29:59 +0300

    index page video

--- site/js/common.js
+++ site/js/common.js
@@ -15,102 +15,76 @@ $(document).ready(function() {
 
 	startScreen();
 
-
-	//detect native scroll events for start screen
-	$('.topper').scroll(function(){
-		var block = $(this);
-		var windowHeight = $('.topper__in').height();
-		var blockscroll = block.scrollTop();
-		var h = block.height();
-
-		scroll = blockscroll + h;
-
-		if (scroll == windowHeight) {
-			$('.topper__in').addClass('is-transformed');
-		}
-		else {
-			$('.topper__in').removeClass('is-transformed');
-		}
-	});	
-
-
-	// throttle
-
-	function throttle(fn, threshhold, scope) {
-	  threshhold || (threshhold = 250);
-	  var last,
-	      deferTimer;
-	  return function () {
-	    var context = scope || this;
-
-	    var now = +new Date,
-	        args = arguments;
-	    if (last && now < last + threshhold) {
-	      // hold on to it
-	      clearTimeout(deferTimer);
-	      deferTimer = setTimeout(function () {
-	        last = now;
-	        fn.apply(context, args);
-	      }, threshhold);
-	    } else {
-	      last = now;
-	      fn.apply(context, args);
-	    }
-	  };
-	}
-
 	//scroll magic animations
+
+	var videoCanvas = document.getElementById('video-explosion');
+
+	function explosion(){
+		var totalImages = 666;
+		var images = new Array();
+			for(var i = 419; i < totalImages; i = i + 3) {
+				var filename = 'boombotix_final_22.09_00';
+				filename += i + '.jpg';
+				var img = new Image;
+				img.src = 'img/explosion/' + filename;
+				images.push(img);
+			}
+			var canv = document.getElementById('explosion');
+			var context = canv.getContext('2d');
+			// $(canv)
+			// 	.width($(window).width())
+			// 	.height($(window).height());
+			var currentLocation = 0;
+			var maxLocation = images.length - 1;
+			wescrolledpast = 0;
+			
+			window.addEventListener('mousewheel', function(e) {
+				// ifelse
+				if(!wescrolledpast){
+					e.preventDefault();
+				}
+				if($(document).scrollTop()<2 && wescrolledpast==1 ){
+					wescrolledpast = 0;
+					console.log('at top');
+					currentLocation = maxLocation-1;
+				}
+				// actual animation
+				if(currentLocation<maxLocation){wescrolledpast = 0}
+				else{wescrolledpast = 1;}
+				if(!wescrolledpast){
+					var delta = Math.max(-1, Math.min(1, e.wheelDelta));
+					if(delta == -1) currentLocation += 1;
+					if(delta == 1) currentLocation -= 1;
+					if(currentLocation < 0) currentLocation = 0;
+					if(currentLocation > maxLocation) currentLocation = maxLocation;
+					setImage(currentLocation);
+
+					// if (currentLocation == 0 && delta == 1) {
+					// 	$('.video_explosion').removeAttr('style');
+					// };
+				}
+			});
+			function setImage(newLocation) {
+				context.drawImage(images[newLocation], 0, 0, 1920, 1080);
+			}
+	}
 
 
 	$(function () { // wait for document ready
 		// init controller
-		var controller = new ScrollMagic.Controller({container: ".topper"});
-		var videoWrap = document.getElementById('video-explosion');
+		var controller = new ScrollMagic.Controller();
 
 		var playForward = new TimelineMax()
-			.to(videoWrap, 0.001, {opacity:1, visibility:'visible'})
-			.add(function(){
-
-				$('.topper').on('scroll', throttle(function (event) {
-					
-					// get video properties
-					var video = document.getElementById('explosion'),
-						videoDuration = video.duration;
-
-						//console.log(videoDuration);
-
-					// get wrapper properties
-					var wrap = $('.topper'),
-						scrollable = $('.topper__in').height() - wrap.height();
-
-						//console.log(scrollable);
-
-					// do seeking
-					var scrollRatio = $('.topper').scrollTop()/scrollable;
-
-					scrollRatio = scrollRatio.toFixed(3);
-
-					console.log(scrollRatio);
-
-					if(isNaN(scrollRatio)) {
-						scrollRatio = 0
-					};
-
-					video.currentTime = scrollRatio*videoDuration;
-
-					
-
-				}, 10));
-
-			});
+			.to(videoCanvas, 0.001, {opacity:1, visibility:'visible'})
+			.call(explosion);
 
 		// build scene
 		var scene = new ScrollMagic.Scene({
-			offset: 1
+			offset: 0
 		})
 			.addTo(controller)
 			.setTween(playForward)
-			.setPin("#video-explosion");
+			//.setPin("#video-explosion");
 	});
 
 	
@@ -119,32 +93,9 @@ $(document).ready(function() {
 
 	//fullpage init
 
-	$('#fullpage').fullpage({
-		//normalScrollElements: '.topper__in'
-	});
-
-
-// $(function() {
-//     $(window)
-//     .on('scroll', vidProgress);
-    
-//     function vidProgress() {
-//         // Get video properties
-//         var $v = document.getElementById('explosion'),
-//             duration = $v.duration;
-        
-//         // Get window properties
-//         var $w = $(window),
-//             scrollable = $(document).height() - $w.height();
-        
-//         // Do seeking
-//         var scrollRatio = $(document).scrollTop()/scrollable;
-//         if(isNaN(scrollRatio)) {scrollRatio = 0};
-//         var progress = scrollRatio*duration;
-//         console.log(progress);
-//         $v.currentTime = progress;
-//     };
-// });
+	// $('#fullpage').fullpage({
+	// 	normalScrollElements: '.topper'
+	// });
 
 
 
